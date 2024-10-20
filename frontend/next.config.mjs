1fbf4e8c7a2d4f32597fcb6af8becf5a2a08c23d
Rahul <bathijarahul@gmail.com>
2024-10-20 15:04:37 -0700

    vercel bug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'standalone', 
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://github-repo-analyzer.onrender.com/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
