ff0c17a5573e1deee594b1e825158bac37358aee
Rahul <bathijarahul@gmail.com>
2024-10-20 22:10:53 -0700

    frontend fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,29 +1,31 @@
 // frontend/app/layout.tsx
 
 import type { Metadata } from "next";
-import { Space_Grotesk } from 'next/font/google';
-import "./globals.css";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";  // Import CSS from `app/` folder
 
-const spaceGrotesk = Space_Grotesk({ 
-  subsets: ['latin'],
-  display: 'swap',
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
 });
+
 
 export const metadata: Metadata = {
   title: "Github Profile Analyzer",
   description: "Get key insights for a Github Profile in seconds.",
+  icons: {
+    icon: "/favicon.ico",  
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className={spaceGrotesk.className}>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
