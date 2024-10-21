4b695d9a4e81071207c0919afa0914579f0d96d5
Rahul <bathijarahul@gmail.com>
2024-10-20 21:20:56 -0700

    configjs update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    // output: 'standalone', 
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://github-repo-analyzer.onrender.com/:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
