78e1d94c9874eef89e2d586d9a08c604ee9e13a6
Rahul <bathijarahul@gmail.com>
2024-10-22 16:17:30 -0700

    mobile styling

--- frontend/components/UserProfile.tsx
+++ frontend/components/UserProfile.tsx
@@ -21,8 +21,17 @@ const UserProfileComponent: React.FC<UserProfileProps> = ({ userProfile }) => {
   }
 
   return (
-    <div className="mt-5 grid grid-cols-6 gap-5 items-center md:grid-cols-6">
-      <div className="col-span-6 md:col-span-5 order-2 md:order-1">
+    <div className="mt-5 grid grid-cols-1 md:grid-cols-6 gap-5 items-start">
+      <div className="col-span-1 md:col-span-1 order-1 mb-4 md:mb-0">
+        <Image
+          src={userProfile.avatarUrl}
+          alt="Avatar"
+          width={100}
+          height={100}
+          className="rounded-full w-24 h-24 md:w-28 md:h-28"
+        />
+      </div>
+      <div className="col-span-1 md:col-span-5 order-2">
         <h2 className="text-3xl md:text-4xl font-bold mb-2">
           {userProfile.name || userProfile.login}
         </h2>
@@ -38,15 +47,6 @@ const UserProfileComponent: React.FC<UserProfileProps> = ({ userProfile }) => {
             <strong>Following:</strong> {userProfile.following}
           </span>
         </p>
-      </div>
-      <div className="col-span-6 md:col-span-1 justify-self-center md:justify-self-end order-1 md:order-2 mb-4 md:mb-0">
-        <Image
-          src={userProfile.avatarUrl}
-          alt="Avatar"
-          width={100}
-          height={100}
-          className="rounded-full w-24 h-24 md:w-28 md:h-28"
-        />
       </div>
     </div>
   );
