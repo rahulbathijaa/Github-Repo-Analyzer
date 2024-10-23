69b6a3be7283c88ffb2e10260a7099f18c30c38f
Rahul <bathijarahul@gmail.com>
2024-10-23 13:12:26 -0700

    cleanup

--- frontend/components/UserProfile.tsx
+++ frontend/components/UserProfile.tsx
@@ -21,17 +21,15 @@ const UserProfileComponent: React.FC<UserProfileProps> = ({ userProfile }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
-      <div className="col-start-2 col-span-1 md:col-span-1">
-        <Image
-          src={userProfile.avatarUrl}
-          alt="Avatar"
-          width={100}
-          height={100}
-          className="rounded-full w-24 h-24 md:w-28 md:h-28 mx-auto md:mx-0"
-        />
-      </div>
-      <div className="col-start-3 col-span-10 md:col-span-10 mt-4 md:mt-0">
+    <div className="grid grid-cols-10">
+      <Image
+        src={userProfile.avatarUrl}
+        alt="Avatar"
+        width={100}
+        height={100}
+        className="col-start-1 rounded-full w-24 h-24 md:w-28 md:h-28"
+      />
+      <div className="col-start-2 col-span-8 flex flex-col">
         <h2 className="text-3xl md:text-4xl font-bold mb-2 text-center md:text-left">
           {userProfile.name || userProfile.login}
         </h2>
